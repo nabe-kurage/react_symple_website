ceacdc2cd49af5e573f9047b3adefa8ae6b8b3f5
nabe-kurage <38372239+nabe-kurage@users.noreply.github.com>
2020-05-16 12:23:15 +0900

    separate components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import WorkItem from "./WorkItem";
+
 import "minireset.css";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1 className="header-title">Portfolio</h1>
-        <nav className="headerNav">
-          <a className="headerNav-item" href="#about">
-            About
-          </a>
-          <a className="headerNav-item" href="#works">
-            Works
-          </a>
-        </nav>
-      </header>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>nabekura ポートフォリオ</title>
+        <meta name="description" content="ポートフォリオサイトのサンプル" />
+      </Helmet>
+      <Header />
       <div className="hero">
         <h1 className="hero-title">Nabekura</h1>
         <p className="hero-description">Web Engineer, Prototyper</p>
@@ -28,29 +29,11 @@ export default function App() {
         <h2 className="section-title">Works</h2>
         <p className="section-description">React Gatsby, Ruby on Rails</p>
         <ul>
-          <li className="workItem">
-            <img
-              className="workItem-image"
-              src="https://picsum.photos/id/101/300/200"
-              alt="成果物１のサプル画像"
-            />
-            <h3 className="workItem-category">Vue.js</h3>
-            <h3 className="workItem-title">成果物２</h3>
-          </li>
-          <li className="workItem">
-            <img
-              className="workItem-image"
-              src="https://picsum.photos/id/1023/300/200"
-              alt="成果物１のサプル画像"
-            />
-            <h3 className="workItem-category">React</h3>
-            <h3 className="workItem-title">成果物１</h3>
-          </li>
+          <WorkItem id="101" title="成果物１" category="React" />
+          <WorkItem id="1023" title="成果物２" category="Vue.js" />
         </ul>
       </section>
-      <footer className="footer">
-        copyright &copy; nabekura. All Rights Reserved.
-      </footer>
+      <Footer />
     </div>
   );
 }
