0a18cb2db15cc3de4e76689672c6e83794fa7768
nabe-kurage <38372239+nabe-kurage@users.noreply.github.com>
2020-05-16 10:26:54 +0900

    making html part

--- src/App.js
+++ src/App.js
@@ -5,26 +5,27 @@ export default function App() {
   return (
     <div className="App">
       <header>
-        <h1>mottox2 Portfolio</h1>
+        <h1>Nabekura Portfolio</h1>
         <nav>
           <a href="#about">About</a>
           <a href="#works">Works</a>
         </nav>
       </header>
       <div>
-        <h1>mottox2</h1>
+        <h1>Nabekura</h1>
         <p>Web Engineer, Prototyper</p>
       </div>
       <section>
         <h2>About</h2>
-        <p>React􏷲Gatsby, Ruby on Rails</p>
+        <p>Web Engineer, Prototyper</p>
       </section>
       <section>
         <h2>Works</h2>
         <p>React Gatsby, Ruby on Rails</p>
         <ul>
           <li>
-            <img src="" /> <h3>成果物1</h3>
+            <img src="" />
+            <h3>成果物1</h3>
           </li>{" "}
           <li>
             <img src="" />
